25143f07410c1044c1942400ea3eb152d958884a
malihsolutions <simon.malih@malihsolutions.co.uk>
2025-09-02 00:15:31 +0100

    add goal mocks

--- Domain/Sources/Models/MatchStatus.swift
+++ Domain/Sources/Models/MatchStatus.swift
@@ -10,18 +10,6 @@ public enum MatchStatus: String, Decodable, Sendable, CaseIterable {
     /// Match has finished
     case completed = "C"
     
-    /// Human-readable description of the match status for debugging and logging
-    public var description: String {
-        switch self {
-        case .upcoming:
-            "Upcoming"
-        case .inProgress:
-            "In Progress"
-        case .completed:
-            "Completed"
-        }
-    }
-    
     public var isLive: Bool {
         self == .inProgress
     }
